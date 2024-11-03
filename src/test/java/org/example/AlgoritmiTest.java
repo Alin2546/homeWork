23f27e452b2625a508a60a204bcc43f05505dbfb
Alin2546 <eternalsq1337%gmail.com>
2024-11-03 16:45:49 +0200

    FirstHomeWorkV3

--- src/test/java/org/example/AlgoritmiTest.java
+++ src/test/java/org/example/AlgoritmiTest.java
@@ -1,14 +1,22 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlgoritmiTest {
+    public static Algoritmi algo;
+    @BeforeAll
+    static void init(){
+        algo = new Algoritmi();
+    }
+
     @Test
     void sumOf100NumbersShouldEqual5050() {
+
     assertEquals(0,1);
     }
 
